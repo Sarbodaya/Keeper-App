71acf130990ac478c9701ec5d5684304a08b4025
Sarbodaya Jena <sarbodayajena2000@gmail.com>
2021-07-14 17:42:28 +0530

    Complete Keeper App Version 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+
+    const d = new Date();
+    const year = d.getFullYear();
+
+    return <footer>
+        <p>Copyright ⓒ {year}</p>
+    </footer>;
+}
+
+export default Footer;
